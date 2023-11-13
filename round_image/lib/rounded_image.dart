6e00f604fc11d8576780810a209d124dcdf7cb44
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-13 19:49:19 +0200

    Returning clip

--- round_image/lib/rounded_image.dart
+++ round_image/lib/rounded_image.dart
@@ -2,4 +2,5 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 /// A function that rounds the corners of an image.
 Widget roundImage(String imagePath, double radius) {
+  return ClipRRect(
  
