158a3939ea19497b4968202bfc11ddd821e873a2
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-18 22:01:32 +0200

    Required widget

--- Stripe-Payments/Test/lib/auth_provider.dart
+++ Stripe-Payments/Test/lib/auth_provider.dart
@@ -5,5 +5,9 @@ class AuthProvider extends InheritedWidget {
   final AuthService auth;
 
   AuthProvider({
- 
+    Key? key,
+    required Widget child,
+  })  : auth = AuthService(),
+        super(key: key, child: child);
+
  
