5962863336e2c80fac00bab7eafe94b2a8078dbf
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-21 23:43:38 +0200

    Handle, public func

--- local_notifications/FlutterLocalNotificationsPlugin.swift
+++ local_notifications/FlutterLocalNotificationsPlugin.swift
@@ -8,3 +8,4 @@ public class SwiftFlutterLocalNotificationsPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
