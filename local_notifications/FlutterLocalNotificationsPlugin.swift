60529057fb0b7c56474d6150dcf79208a41f8b21
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-21 23:43:21 +0200

    adding method call delegate

--- local_notifications/FlutterLocalNotificationsPlugin.swift
+++ local_notifications/FlutterLocalNotificationsPlugin.swift
@@ -5,3 +5,6 @@ public class SwiftFlutterLocalNotificationsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_local_notifications", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterLocalNotificationsPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
