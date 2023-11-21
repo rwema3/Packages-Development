e0ff49c26a52ea2eed7a0c0f1010490d26456aa6
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-21 23:43:03 +0200

    Let channel

--- local_notifications/FlutterLocalNotificationsPlugin.swift
+++ local_notifications/FlutterLocalNotificationsPlugin.swift
@@ -3,3 +3,4 @@ import UIKit
 
 public class SwiftFlutterLocalNotificationsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_local_notifications", binaryMessenger: registrar.messenger())
